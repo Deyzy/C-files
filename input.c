97a3ea707927a0ed34b0a042d1ab63377777440e
deyzy <deisesantos487@gmail.com>
2025-01-09 13:35:51 -0300

    update

--- input.c
+++ input.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    
 
+    char nome[25];
+    int age;
 
+    printf("Qual o seu nome? ");
+    fgets(nome, 25, stdin);  /* Função fgets: lê uma linha de texto da entrada padrão (stdin), ou seja, do teclado, e armazena essa linha na variável nome */
+    nome[strlen(nome)-1] = '\0';
+
+    printf("Quantos anos você tem? ");
+    scanf("%d", &age); //scanf não ler espaços em brancos
+
+    printf("\nHello %s, como você está?",nome);
+    printf("\nyou are %d years old\n", age);
 
     return 0;
 }
