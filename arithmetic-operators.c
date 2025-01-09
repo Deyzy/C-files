252bd48170e1ea6fad7074cf95ecbee5fa9e9a1e
deyzy <deisesantos487@gmail.com>
2025-01-09 11:18:58 -0300

    update

---
+++ arithmetic-operators.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    //operações aritmeticas 
+
+    // ++ incremente 
+    // -- decrement 
+
+    int x = 5;
+    int y = 2;
+
+    // int a = x + y;
+    // printf ("a :%d\n", a);
+
+    // int b = x - y;
+    // printf ("b :%d\n", b);
+
+    // int c = x * y;
+    // printf ("c :%d\n", c);
+
+    // float d = x / (float)y;
+    // printf ("d :%f\n", d);
+
+    // int e = x % y;
+    // printf ("e :%d\n", e);
+
+    x++;
+    y--;
+
+    printf("%d\n", x);
+    printf("%d\n", y);
+
+    return 0;
+}
---
+++ constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    // constante = valor fixo que não pode ser alterado durante a execução
+    
+    const float PI = 3.14159;
+
+    printf("%f", PI);
+
+    return 0;
+}
